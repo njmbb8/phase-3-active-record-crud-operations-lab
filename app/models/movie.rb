ed7a6cf0d91f6de1a7bd6408b80322e466d297a8
njmbb8 <nmcgr001@gmail.com>
2021-11-07 18:47:25 -0500

    Completed Assignment

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,49 @@
-class Movie < ActiveRecord::Base
+require 'pry'
 
+class Movie < ActiveRecord::Base
+    def self.create_with_title(title)
+        new_movie = Movie.new(title: title)
+        new_movie.save
+        return new_movie
+    end
+
+    def self.first_movie
+        Movie.first
+    end
+
+    def self.last_movie
+        Movie.last
+    end
+
+    def self.movie_count
+        Movie.all.size
+    end
+
+    def self.find_movie_with_id(id)
+        Movie.find(id)
+    end
+
+    def self.find_movie_with_attributes(args)
+        Movie.find_by(args)
+    end
+
+    def self.find_movies_after_2002
+        Movie.where("release_date > 2002")
+    end
+
+    def update_with_attributes(args)
+        self.update(args)
+    end
+
+    def self.update_all_titles(title)
+        Movie.update(title: title)
+    end
+
+    def self.delete_by_id(id)
+        Movie.find(id).destroy
+    end
+
+    def self.delete_all_movies
+        Movie.destroy_all
+    end
 end
